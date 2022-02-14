7f46d273ddaa10157f42bed44cccc691b885d785
eugene663 <yujin663@gmail.com>
2022-02-14 18:05:36 +0900

    upload day6

---
+++ quiz6.js
@@ -0,0 +1,46 @@
+var x = 10;
+var y = 3;
+console.log("x =",x, "y =", y);
+console.log("\n");
+console.log("산술 연산자 +, - , *, / , %");
+console.log("x + y = ", x+y);
+console.log("x - y = ", x-y);
+console.log("x * y = ", x*y);
+console.log("x / y = ", x/y);
+console.log("x % y = ", x%y);
+console.log("\n");
+
+console.log("대입 연산자");
+console.log("x += y -> x = ", x+=y);
+console.log("x -= y -> x = ", x-=y);
+console.log("x *= y -> x = ", x*=y);
+console.log("x /= y -> x = ", x/=y);
+console.log("x %= y -> x = ", x%=y);
+console.log("\n");
+
+console.log("증감 연산자");
+console.log("x++ -> x = ", x++);
+console.log("x-- -> x = ", x--);
+console.log("++x -> x = ", ++x);
+console.log("--x -> x = ", --x);
+console.log("\n");
+
+var x = 10;
+var y = 12;
+var str1 = "10";
+console.log("비교 연산자");
+console.log("x =", x," y =", y, "str1 =", str1);
+console.log("x == y ->", x==y);
+console.log("x != y ->", x!=y);
+console.log("x >= y ->", x>=y);
+console.log("x <= y ->", x<=y);
+console.log("x == str1 ->", x==str1);
+console.log("x === str1 ->", x===str1);
+console.log("x !== str1 ->", x!==str1);
+console.log("\n");
+
+console.log("논리 연산자");
+console.log("and: &&");
+console.log("or: ||");
+console.log("역: !");
+
